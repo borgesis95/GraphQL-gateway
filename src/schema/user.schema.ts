1584efbcde955236350287622b563988cb34113e
Salvatore <borgesisalvo@gmail.com>
2021-05-18 21:51:56 +0200

    feat(all): add apollo-server-cache

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,13 +1,21 @@
-import {gql} from 'apollo-server-express';
+import { gql } from "apollo-server-express";
 
 export default gql`
   type User {
-    """ user's id """
+    """
+     user's id
+    """
     id: ID
-    """ user email """
-    email: String,
+    """
+     user email
+    """
+    email: String
 
-    """ this is the JWT getting back for login operation """
-    token : String
+    """
+     this is the JWT getting back for login operation
+    """
+    token: String
   }
-  `
+
+  
+`;
